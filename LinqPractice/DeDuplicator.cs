44a08108f153442110456c524d5c149fd2894397
Trenton Daniels <trentondaniels95@gmail.com>
2018-08-20 09:57:25 -0500

    completed remove duplicate function

---
+++ LinqPractice/DeDuplicator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqPractice
+{
+    public class DeDuplicator
+    {
+        public DeDuplicator()
+        {
+        }
+
+        public IEnumerable<string> RemoveDuplicates(List<string> list)
+        {
+            var listWithoutDuplicates = list.OrderBy(word=> word).Where(word => RemoveItem(list, word).Contains(word) == false);
+            return listWithoutDuplicates;
+        }
+
+        private List<string> RemoveItem(List<string> list, string word) 
+        {
+            list.Remove(word);
+            return list;
+        }
+    }
+}
--- LinqPractice/Program.cs
+++ LinqPractice/Program.cs
@@ -7,12 +7,20 @@ namespace LinqPractice
     {
         public static void Main(string[] args)
         {
-            List<string> listOfWords = new List<string>() {"the", "bike", "this", "it", "tenth", "mathematics"}
+            List<string> listOfWords = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
             IEnumerable<string> wordsWithTH = new SubstringTH().FindTH(listOfWords);
             Console.WriteLine("Problem 1:");
             foreach (string word in wordsWithTH)
             {
                 Console.WriteLine(word);
+            }
+
+            List<string> listWithDuplicates = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+            IEnumerable<string> listWithoutDuplicates = new DeDuplicator().RemoveDuplicates(listWithDuplicates);
+            Console.WriteLine("Problem 2:");
+            foreach (string name in listWithoutDuplicates)
+            {
+                Console.WriteLine(name);
             }
 
 
